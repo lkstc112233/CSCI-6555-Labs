d78f71779a364977eddf3ed50fd47c0e3b51095d
Photon Cat <lkstc112233@gmail.com>
2018-09-14 14:29:48 -0400

    updated some formats

--- Lab1/Camera/Camera.h
+++ Lab1/Camera/Camera.h
@@ -3,26 +3,27 @@
 
 #include <glm/glm.hpp>
 
-class Camera {
-private:
+class Camera
+{
+  private:
     const glm::vec3 up = glm::vec3(0, 1, 0);
     bool lock = false;
     glm::vec3 position = glm::vec3(0);
     glm::vec3 front = glm::vec3(0, 0, 1);
     glm::vec3 target = glm::vec3(0);
-public:
     void setPosition(const glm::vec3& position);
-    void move(const glm::vec3& direction);
+  public:
+    void move(const glm::vec3 &direction);
     void moveForward(float distance);
     void moveBackward(float distance);
     void moveLeft(float distance);
     void moveRight(float distance);
     void moveUp(float distance);
     void moveDown(float distance);
-    void lookAt(const glm::vec3& position);
-    void lockView(const glm::vec3& position);
+    void lookAt(const glm::vec3 &position);
+    void lockView(const glm::vec3 &position);
     void unlockView();
-    bool isViewLocked() {return lock;}
+    bool isViewLocked() { return lock; }
     glm::mat4 getViewMat();
 };
 
