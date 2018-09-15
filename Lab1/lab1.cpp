edae152441361beefdb4ed2792eee38ca4d49190
Photon Cat <lkstc112233@gmail.com>
2018-09-15 19:47:27 -0400

    Applied model in lab1

--- Lab1/lab1.cpp
+++ Lab1/lab1.cpp
@@ -12,6 +12,7 @@
 #include "Shaders/Shader.h"
 #include "Texture/Texture.h"
 #include "Camera/Camera.h"
+#include "Models/Model.h"
 
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
@@ -32,9 +33,6 @@ void processInput(GLFWwindow *window)
 	{
 		glm::mat4 teapotTrans(1.0f);
 		static float angle = 0;
-		// teapotTrans = glm::translate(teapotTrans, glm::vec3(0, -0.3, 0));
-		// teapotTrans = glm::translate(teapotTrans, glm::vec3(0, 0, 0.6));
-		// teapotTrans = glm::rotate(teapotTrans, glm::radians(-45.0f), glm::vec3(1.0, 0.0, 0.0));
 		teapotTrans = glm::rotate(teapotTrans, float(angle += M_PI / 60.), glm::normalize(glm::vec3(1.0, 1.0, 1.0)));
 		program->setMatrix("transform", teapotTrans);
 	}
@@ -127,6 +125,8 @@ int main()
 	glEnable(GL_DEPTH_TEST);
 	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
+	Model cube = ModelLoader::loadFile("res/models/cube.off");
+
 	ShaderProgram shaderProgram{
 		Shader::createVertexShader("res/shaders/simpleShader.vert"),
 		Shader::createFragmentShader("res/shaders/simpleShader.frag")};
@@ -134,69 +134,13 @@ int main()
 	{
 		return -4;
 	}
-
-	ShaderProgram teapotShaderProgram{
-		Shader::createVertexShader("res/shaders/cubeShader.vert"),
-		Shader::createFragmentShader("res/shaders/cubeShader.frag")};
-	if (!teapotShaderProgram.isValid())
-	{
-		return -4;
-	}
-	program = &teapotShaderProgram;
-	teapotShaderProgram.setMatrix("transform", glm::mat4(1.0f));
-
-	unsigned int teapotVao;
-	glGenVertexArrays(1, &teapotVao);
-	unsigned int teapotVbo;
-	glGenBuffers(1, &teapotVbo);
-	unsigned int EBO, teapotEbo;
-	glGenBuffers(1, &teapotEbo);
-
-#include "cube.h"
-	glBindVertexArray(teapotVao);
-	glBindBuffer(GL_ARRAY_BUFFER, teapotVbo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(teapotVertices), teapotVertices, GL_STATIC_DRAW);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, teapotEbo);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(teapotIndices), teapotIndices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
-	teapotShaderProgram.use();
-	teapotShaderProgram.setValue("texture0", 0);
-	teapotShaderProgram.setValue("texture1", 1);
-	teapotShaderProgram.setValue("ratio", 0.2f);
-
-	unsigned int lightVao;
-	glGenVertexArrays(1, &lightVao);
-	glBindVertexArray(lightVao);
-	glBindBuffer(GL_ARRAY_BUFFER, teapotVbo);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, teapotEbo);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
-
-	ShaderProgram lightShaderProgram {
-		Shader::createVertexShader("res/shaders/cubeShader.vert"),
-		Shader::createFragmentShader("res/shaders/lightShader.frag")};
-	if (!lightShaderProgram.isValid())
-	{
-		return -4;
-	}
-	glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
-	glm::mat4 lightTransform = glm::mat4(1.0f);
-	lightTransform = glm::translate(lightTransform, lightPos);
-	lightTransform = glm::scale(lightTransform, glm::vec3(0.2f)); 
-	lightShaderProgram.setMatrix("transform", lightTransform);
+	program = &shaderProgram;
+	shaderProgram.setMatrix("transform", glm::mat4(1.0f));
 
 	glm::mat4 projection(1.0f);
 	projection = glm::perspective(glm::radians(45.0f), PROJECTION_RATIO, 0.1f, 100.0f);
 
-	teapotShaderProgram.setMatrix("projection", projection);
-	lightShaderProgram.setMatrix("projection", projection);
-
-	teapotShaderProgram.setVector("lightColor", glm::vec3(1.0f));
+	shaderProgram.setMatrix("projection", projection);
 
 	Camera camera;
 	activeCamera = &camera;
@@ -210,19 +154,9 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		float ratioValue = (sin(glfwGetTime()) / 2.0f) + 0.5f;
-		shaderProgram.setValue("ratio", ratioValue);
-
-		glBindVertexArray(teapotVao);
-		teapotShaderProgram.use();
-		teapotShaderProgram.setMatrix("view", camera.getViewMat());
-		teapotShaderProgram.setVector("viewPos", camera.position);
-		glDrawElements(GL_TRIANGLES, sizeof(teapotIndices) / sizeof(unsigned), GL_UNSIGNED_INT, 0);
-
-		glBindVertexArray(lightVao);
-		lightShaderProgram.use();
-		lightShaderProgram.setMatrix("view", camera.getViewMat());
-		glDrawElements(GL_TRIANGLES, sizeof(teapotIndices) / sizeof(unsigned), GL_UNSIGNED_INT, 0);
+		shaderProgram.use();
+		shaderProgram.setMatrix("view", camera.getViewMat());
+		cube.draw(shaderProgram);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
