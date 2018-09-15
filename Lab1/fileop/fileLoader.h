07a5f915c447105d303d730f62efa59ad1bfa5f2
Photon Cat <lkstc112233@gmail.com>
2018-09-15 16:12:18 -0400

    Formation change

--- Lab1/fileop/fileLoader.h
+++ Lab1/fileop/fileLoader.h
@@ -7,12 +7,12 @@
 
 class FileLoader
 {
-  private:
+private:
 	std::ifstream fileStream;
 	std::istream &file;
 	char *data = nullptr;
 
-  public:
+public:
 	FileLoader(std::istream &ifs) : file(ifs) {}
 	FileLoader(const char *filename) : fileStream(filename), file(fileStream) {}
 	FileLoader(const std::string filename) : fileStream(filename), file(fileStream) {}
