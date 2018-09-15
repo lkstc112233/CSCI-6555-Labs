b3a36f5f90f733b91b28217e2cbe71ee6e3467e4
Photon Cat <lkstc112233@gmail.com>
2018-09-15 16:38:55 -0400

    Added some code handling buffers

--- Lab1/Models/Model.cpp
+++ Lab1/Models/Model.cpp
@@ -1,6 +1,31 @@
 #include "Model.h"
 
-Model::~Model() {
+#include <glad/glad.h>
+
+Model::Model()
+{
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+}
+
+Model::Model(Model &&model)
+{
+    vertexes = model.vertexes;
+    indices = model.indices;
+    VAO = model.VAO;
+    VBO = model.VBO;
+    EBO = model.EBO;
+    model.vertexes = nullptr;
+    model.indices = nullptr;
+}
+
+Model::~Model()
+{
     delete[] vertexes;
     delete[] indices;
+}
+
+Model ModelLoader::loadFile(const char *filename)
+{
 }
--- Lab1/Models/Model.h
+++ Lab1/Models/Model.h
@@ -6,11 +6,23 @@
 class Model
 {
   private:
+    friend class ModelLoader;
     float *vertexes = nullptr;
     int *indices = nullptr;
+    unsigned VAO, VBO, EBO;
+    Model();
+    Model(const Model &) = delete;
+    Model &operator=(const Model &) = delete;
+
   public:
     ~Model();
+    Model(Model &&);
 };
 
+class ModelLoader
+{
+  public:
+    static Model loadFile(const char *filename);
+};
 
 #endif // MODELS_MODEL_H
