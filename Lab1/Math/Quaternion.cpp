335c57edb65d4ecb64364589baa6ca0d283e7c54
Photon Cat <lkstc112233@gmail.com>
2018-09-15 20:57:11 -0400

    Added more quaternion operations

--- Lab1/Math/Quaternion.cpp
+++ Lab1/Math/Quaternion.cpp
@@ -10,6 +10,78 @@ Quaternion::Quaternion(float wi, float xi, float yi, float zi)
     normalize();
 }
 
+Quaternion &Quaternion::operator+=(const Quaternion &op2)
+{
+    w += op2.w;
+    x += op2.x;
+    y += op2.y;
+    z += op2.z;
+    normalize();
+    return *this;
+}
+Quaternion &Quaternion::operator-=(const Quaternion &op2)
+{
+    w -= op2.w;
+    x -= op2.x;
+    y -= op2.y;
+    z -= op2.z;
+    normalize();
+    return *this;
+}
+Quaternion &Quaternion::operator*=(const Quaternion &op2)
+{
+    w = w * op2.w - x * op2.x - y * op2.y - z * op2.z;
+    x = w * op2.x + x * op2.w + y * op2.z - z * op2.y;
+    y = w * op2.y - x * op2.z + y * op2.w + z * op2.x;
+    z = w * op2.z + x * op2.y - y * op2.x + z * op2.w;
+    normalize();
+    return *this;
+}
+Quaternion &Quaternion::operator*=(float op2)
+{
+    w *= op2;
+    x *= op2;
+    y *= op2;
+    z *= op2;
+    normalize();
+    return *this;
+}
+Quaternion &Quaternion::operator/=(float op2)
+{
+    w /= op2;
+    x /= op2;
+    y /= op2;
+    z /= op2;
+    normalize();
+    return *this;
+}
+
+Quaternion Quaternion::operator+(const Quaternion &op2) const
+{
+    Quaternion result(*this);
+    return result += op2;
+}
+Quaternion Quaternion::operator-(const Quaternion &op2) const
+{
+    Quaternion result(*this);
+    return result -= op2;
+}
+Quaternion Quaternion::operator*(const Quaternion &op2)const
+{
+    Quaternion result(*this);
+    return result *= op2;
+}
+Quaternion Quaternion::operator*(float op2)const
+{
+    Quaternion result(*this);
+    return result *= op2;
+}
+Quaternion Quaternion::operator/(float op2) const
+{
+    Quaternion result(*this);
+    return result /= op2;
+}
+
 void Quaternion::normalize()
 {
     float d = sqrt(w * w + x * x + y * y + z * z);
--- Lab1/Math/Quaternion.h
+++ Lab1/Math/Quaternion.h
@@ -13,6 +13,18 @@ class Quaternion
     Quaternion();
     Quaternion(float w, float x, float y, float z);
 
+    Quaternion &operator+=(const Quaternion &);
+    Quaternion &operator-=(const Quaternion &);
+    Quaternion &operator*=(const Quaternion &);
+    Quaternion &operator*=(float);
+    Quaternion &operator/=(float);
+
+    Quaternion operator+(const Quaternion &) const;
+    Quaternion operator-(const Quaternion &) const;
+    Quaternion operator*(const Quaternion &) const;
+    Quaternion operator*(float) const;
+    Quaternion operator/(float) const;
+
     void normalize();
 };
 
