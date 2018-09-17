3e13dae7d1564d477fe8c46019102ab38164a63f
Photon Cat <lkstc112233@gmail.com>
2018-09-17 16:29:44 -0400

    Implemented keyframe interpolating

--- Lab1/Animate/Scripts.cpp
+++ Lab1/Animate/Scripts.cpp
@@ -19,7 +19,24 @@ template <typename T>
 glm::mat4 ScriptsImplementation<T>::getTranscationMatrixAt(float time)
 {
     // TODO: implement keyframe speed.
+    // For now it's only uniform, repeating.
 
+    int maximum = keyframes.size() - 3;
+    if (maximum <= 3)
+    {
+        // Too less keyframes. Just return what's available.
+        if (keyframes.size())
+        {
+            return keyframes[0].getTranscationMatrix();
+        }
+        return glm::mat4(1.0f);
+    }
+
+    float frame = fmod(time, maximum);
+    float t = fmod(frame, 1.0);
+    int indexBegin = frame - 1;
+    
+    return catmullRomInterpolate(t, keyframes.begin() + indexBegin).getTranscationMatrix();
 }
 
 std::unique_ptr<Scripts> ScriptsLoader::loadScript(const char *filename)
