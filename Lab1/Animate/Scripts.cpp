3dc14a083b9259f5af034a95486a1f38a740799b
Photon Cat <lkstc112233@gmail.com>
2018-09-17 15:54:01 -0400

    Implemented a template inherence

--- Lab1/Animate/Scripts.cpp
+++ Lab1/Animate/Scripts.cpp
@@ -1,1 +1,7 @@
 #include "Scripts.h"
+
+#include <memory>
+std::unique_ptr<Scripts> ScriptsLoader::loadScript(const char *filename)
+{
+    
+}
--- Lab1/Animate/Scripts.h
+++ Lab1/Animate/Scripts.h
@@ -1,8 +1,24 @@
 #ifndef ANIMATE_SCRIPTS_H
 #define ANIMATE_SCRIPTS_H
+
+#include <memory>
 class Scripts
 {
   public:
     virtual ~Scripts();
 };
+
+template <typename T>
+class ScriptsImplementation : public Scripts
+{
+  private:
+    friend class ScriptsLoader;
+};
+
+class ScriptsLoader
+{
+  public:
+    static std::unique_ptr<Scripts> loadScript(const char *filename);
+};
+
 #endif // ANIMATE_SCRIPTS_H
