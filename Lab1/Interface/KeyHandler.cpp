026aa28a4b074fded8d8f1e5b88bd474388c3f11
Quantum cat <lkstc112233@users.noreply.github.com>
2018-09-18 04:01:12 -0400

    Implemented a key handler

--- Lab1/Interface/KeyHandler.cpp
+++ Lab1/Interface/KeyHandler.cpp
@@ -1,1 +1,25 @@
 #include "KeyHandler.h"
+
+#include <glad/glad.h>
+
+KeyHandler::KeyHandler(GLFWwindow *windowi, int keyListeningi, decltype(handler) handleri, bool pressOnly)
+	: window(windowi)
+	, keyListening(keyListeningi)
+	, handler(handleri)
+	, pressOnly(pressOnlyi)
+{
+}
+
+void KeyHandler::handle() {
+	if (glfwGetKey(window, keyListening) == GLFW_PRESS) {
+		if (!(pressOnly && pressed)) {
+			handler(true);
+		}
+		pressed = true;
+	} else {
+		if (!pressOnly || pressed)) {
+			handler(false);
+		}
+		pressed = false;
+	}
+}
--- Lab1/Interface/KeyHandler.h
+++ Lab1/Interface/KeyHandler.h
@@ -1,6 +1,19 @@
 #ifndef INTERFACE_KEYHANDLER_H
 #define INTERFACE_KEYHANDLER_H
 
+#include <functional>
 
+class KeyHandler {
+private:
+	GLFWwindow *window;
+	int keyListening;
+	// argument: bool: isKeyPressed.
+	std::function<void(bool)> handler;
+	bool pressOnly;
+	bool pressed = false;
+public:
+	KeyHandler(GLFWwindow *window, int keyListening, decltype(handler), bool pressOnly = false);
+	void handle();
+};
 
 #endif // INTERFACE_KEYHANDLER_H
