372b102a9c4242fc5a4e154e89d859eddb2c1a22
Photon Cat <lkstc112233@gmail.com>
2018-09-18 17:43:04 -0400

    Implemented a simple Handler stub.

--- Lab1/Interface/MouseHandler.h
+++ Lab1/Interface/MouseHandler.h
@@ -1,5 +1,22 @@
 #ifndef INTERFACE_MOUSEHANDLER_H
 #define INTERFACE_MOUSEHANDLER_H
 
+#include <functional>
+
+enum MouseConstants
+{
+    MOUSE_LEFTBUTTON_PRESSED = 1,
+    MOUSE_RIGHTBUTTON_PRESSED = 2,
+    MOUSE_SCROLLED_UP_PRESSED = 4,
+    MOUSE_SCROLLED_DOWN_PRESSED = 8,
+};
+
+class MouseHandler
+{
+  private:
+    std::function<void(int mouseConstants, float x, float y)> handleEvent;
+
+  public:
+};
 
 #endif // INTERFACE_MOUSEHANDLER_H
