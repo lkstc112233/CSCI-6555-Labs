a3abb7967616b8523b17e3fef13065efdd4fd845
Photon Cat <lkstc112233@gmail.com>
2018-09-23 02:39:36 -0400

    Implemented two state button.

--- Lab1/Interface/Utilities/TwoStateButton.cpp
+++ Lab1/Interface/Utilities/TwoStateButton.cpp
@@ -1,1 +1,45 @@
 #include "TwoStateButton.h"
+
+#include <cmath>
+
+#include "../../Graphics/WindowInitializer.h"
+#include "../../Graphics/Object/Object.h"
+
+TwoStateButton::TwoStateButton(const char* shapeOn, const char* shapeOff, float xi, float yi, float sizei, float rotation)
+    : onStateShape(onStateModel)
+    , offStateShape(offStateModel)
+    , onStateModel(ModelLoader::loadShpFile(shapeOn))
+    , offStateModel(ModelLoader::loadShpFile(shapeOff))
+    , x(xi)
+    , y(yi)
+    , size(sizei)
+{
+	glm::mat3 buttonTransform {
+        {size * cos(rotation), size * sin(rotation), 0},
+        {-size * sin(rotation), size * cos(rotation), 0}, 
+        {x + size / 2, y + size / 2, 1}};
+	onStateShape.setTransformMatrix(buttonTransform);
+	offStateShape.setTransformMatrix(buttonTransform);
+}
+
+void TwoStateButton::attachControls(MouseHandlerContainer& mouseContainer) {
+    mouseContainer.emplace_handler([this](int mouseFlags, float clampedx, float clampedy) {
+		float xc = clampedx / SCREEN_WIDTH;
+		float yc = clampedy / SCREEN_HEIGHT;
+		if ((mouseFlags & MOUSE_LEFTBUTTON_PRESSED) 
+            && xc < x + size 
+            && xc > x 
+            && yc < y + size 
+            && yc > y) {
+			state = !state;
+		}
+	});
+}
+
+void TwoStateButton::draw(ShaderProgram &shader) {
+    if (state) {
+        onStateShape.draw(shader);
+    } else {
+        offStateShape.draw(shader);
+    }
+}
--- Lab1/Interface/Utilities/TwoStateButton.h
+++ Lab1/Interface/Utilities/TwoStateButton.h
@@ -1,9 +1,33 @@
 #ifndef INTERFACE_UTILITIES_TWOSTATEBUTTON_H
 #define INTERFACE_UTILITIES_TWOSTATEBUTTON_H
 
-class twoStateButton {
+#include "../../Graphics/Object/Object.h"
+#include "../KeyHandler.h"
+#include "../MouseHandler.h"
+
+/**
+ * A button that has two states that accepts two shapes as state, manages a state bool.
+ * The button is located on x, y, x + size, y + size. If the shape model is larger than this, 
+ * overflowing part will be drawn but not accepting any click.
+ * On click the button will trigger and the state will flip. If two buttons overlap, they will
+ * both trigger.
+ * 
+ * A rotation can be accepted that will apply to both shapes. 
+ */
+class TwoStateButton {
 private:
+    const Model onStateModel;
+    const Model offStateModel;
+	Object2D onStateShape;
+	Object2D offStateShape;
+    float x;
+    float y;
+    float size;
 public:
+    TwoStateButton(const char* shapeOn, const char* shapeOff, float x, float y, float size, float rotation = 0);
+    bool state = true;
+    void attachControls(MouseHandlerContainer& mouseContainer);
+	void draw(ShaderProgram& shader);
 };
 
 #endif // INTERFACE_UTILITIES_TWOSTATEBUTTON_H
