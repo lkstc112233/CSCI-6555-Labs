3dd213f8a98dbb929ff29d842396592414949a2f
Photon Cat <lkstc112233@gmail.com>
2018-11-11 17:41:24 -0500

    Added world stub

--- Lab3/Physics/World.cpp
+++ Lab3/Physics/World.cpp
@@ -1,1 +1,7 @@
 #include "World.h"
+
+void World::emplaceControllers(Entity& entity) {
+  for (auto e : entity) {
+    controllers.emplace_back(e);
+  }
+}
--- Lab3/Physics/World.h
+++ Lab3/Physics/World.h
@@ -1,4 +1,18 @@
 #ifndef PHYSICS_WORLD_H
 #define PHYSICS_WORLD_H
 
+#include <vector>
+
+#include "../Graphics/Entity/Entity.h"
+
+#include "Controller.h"
+
+class World {
+ private:
+  std::vector<Controller> controllers;
+
+ public:
+  void emplaceControllers(Entity& entity);
+};
+
 #endif  // PHYSICS_WORLD_H
