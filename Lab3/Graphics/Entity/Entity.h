283e6064c8a9fb4fc357af26e570b73869212b80
Photon Cat <lkstc112233@gmail.com>
2018-11-11 17:39:39 -0500

    Made entity for_each-able

--- Lab3/Graphics/Entity/Entity.h
+++ Lab3/Graphics/Entity/Entity.h
@@ -9,10 +9,44 @@
 
 class Entity {
  private:
+  typedef std::map<std::string, std::shared_ptr<Object3D>> MapType;
   // A map for all the objects in this hierarchical structure.
-  std::map<std::string, std::shared_ptr<Object3D>> objects;
+  MapType objects;
 
  public:
+  class iterator {
+   private:
+    MapType::iterator m_iterator;
+    friend class Entity;
+
+   public:
+    iterator& operator++() {
+      ++m_iterator;
+      return *this;
+    }
+    iterator operator++(int) {
+      auto temp = *this;
+      m_iterator++;
+      return temp;
+    }
+    iterator& operator--() {
+      --m_iterator;
+      return *this;
+    }
+    iterator operator--(int) {
+      auto temp = *this;
+      m_iterator--;
+      return temp;
+    }
+    bool operator==(const iterator& another) const {
+      return m_iterator == another.m_iterator;
+    }
+    bool operator!=(const iterator& another) const {
+      return m_iterator != another.m_iterator;
+    }
+    std::shared_ptr<Object3D> operator*() { return m_iterator->second; }
+    Object3D* operator->() { return m_iterator->second.operator->(); }
+  };
   Entity() {}
   template <class... Args>
   void addObject(std::string name, Args&&... args) {
@@ -28,6 +62,16 @@ class Entity {
   }
   void updateManagers(float time);
   void draw(ShaderProgram& shader);
+  iterator begin() {
+    iterator i;
+    i.m_iterator = objects.begin();
+    return i;
+  }
+  iterator end() {
+    iterator i;
+    i.m_iterator = objects.end();
+    return i;
+  }
 };
 
 #endif  // GRAPHICS_ENTITY_ENTITY_H
