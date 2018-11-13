c101edee6c453c4e75dad83bc6f5ef2711df5629
Photon Cat <lkstc112233@gmail.com>
2018-11-12 19:48:24 -0500

    Added stub in Wall

--- Lab3/Physics/Wall.cpp
+++ Lab3/Physics/Wall.cpp
@@ -1,1 +1,4 @@
 #include "Wall.h"
+
+Wall::Wall(glm::vec3 positioni, glm::vec3 normali)
+    : position(positioni), normal(normali) {}
--- Lab3/Physics/Wall.h
+++ Lab3/Physics/Wall.h
@@ -1,4 +1,15 @@
 #ifndef PHYSICS_WALL_H
 #define PHYSICS_WALL_H
 
+#include <glm/glm.hpp>
+
+class Wall {
+ private:
+  glm::vec3 position;
+  glm::vec3 normal;
+
+ public:
+  Wall(glm::vec3 position, glm::vec3 normal);
+};
+
 #endif  // PHYSICS_WALL_H
