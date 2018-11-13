2d12c511a0006f92a594fd9d14fbbfe8b1e76d93
Photon Cat <lkstc112233@gmail.com>
2018-11-13 18:34:07 -0500

    Added interaction of walls and objects.

--- Lab3/Physics/World.cpp
+++ Lab3/Physics/World.cpp
@@ -13,6 +13,13 @@ void World::emplaceControllers(Entity& entity) {
 void World::timePass(float t) {
   for (auto& c : controllers) {
     c.speed += t * g;
+  }
+  for (auto& w : walls) {
+    for (auto& c : controllers) {
+      c.speed += t * w.applyForce(c.position) / c.mass;
+    }
+  }
+  for (auto& c : controllers) {
     c.applyChange(t);
   }
 }
