80553da3ed69e5d70552441058c5bbe742c8d153
Photon Cat <lkstc112233@gmail.com>
2018-11-13 18:08:23 -0500

    Fixed the time reverse bug

--- Lab3/lab3.cpp
+++ Lab3/lab3.cpp
@@ -101,7 +101,7 @@ int main(int argc, char** argv) {
     mouseHandlers.handle();
 
     double thisTime = glfwGetTime();
-    float dtime = lastTime - thisTime;
+    float dtime = thisTime - lastTime;
     lastTime = thisTime;
 
     world.timePass(dtime);
