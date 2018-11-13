9b30ab373b776f0e71703942c1d49fdd38996ebf
Photon Cat <lkstc112233@gmail.com>
2018-11-13 18:31:02 -0500

    Rewrote the apply force function to introduce the k parameter.

--- Lab3/Physics/Wall.cpp
+++ Lab3/Physics/Wall.cpp
@@ -5,11 +5,14 @@
 Wall::Wall(glm::vec3 positioni, glm::vec3 normali)
     : position(positioni), normal(normali) {}
 
+const static float k = 100.0;
+
 glm::vec3 Wall::applyForce(glm::vec3 pos) {
   auto dpos = position - pos;
-  float force = -glm::dot(dpos, normal);
-  if (force < 0) {
+  float force = glm::dot(dpos, normal);
+  if (force <= 0) {
     return glm::vec3(0);
   }
-  return -force / glm::length(normal) * normal;
+  // F = kx
+  return k * force / glm::length(normal) * normal;
 }
