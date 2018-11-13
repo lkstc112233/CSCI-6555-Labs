9c2844ac4d4d21ff56bd0045d6c234ce84a1c8d4
Photon Cat <lkstc112233@gmail.com>
2018-11-13 17:42:05 -0500

    Implemented applyForce

--- Lab3/Physics/Wall.cpp
+++ Lab3/Physics/Wall.cpp
@@ -5,4 +5,11 @@
 Wall::Wall(glm::vec3 positioni, glm::vec3 normali)
     : position(positioni), normal(normali) {}
 
-glm::vec3 Wall::applyForce(glm::vec3 pos) {}
+glm::vec3 Wall::applyForce(glm::vec3 pos) {
+  auto dpos = position - pos;
+  float force = -glm::dot(dpos, normal);
+  if (force < 0) {
+    return glm::vec3(0);
+  }
+  return force / glm::abs(normal) * normal;
+}
