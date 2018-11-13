b5043556057adbbaade004279b4657a8c2d57860
Photon Cat <lkstc112233@gmail.com>
2018-11-12 19:55:44 -0500

    Added an addWall function in world as well as the storage for walls

--- Lab3/Physics/World.h
+++ Lab3/Physics/World.h
@@ -6,13 +6,19 @@
 #include "../Graphics/Entity/Entity.h"
 
 #include "Controller.h"
+#include "Wall.h"
 
 class World {
  private:
   std::vector<Controller> controllers;
+  std::vector<Wall> walls;
   static const glm::vec3 g;
 
  public:
+  template <typename... Args>
+  void addWall(Args... args) {
+    walls.emplace_back(args...);
+  }
   void emplaceControllers(Entity& entity);
   void timePass(float t);
 };
