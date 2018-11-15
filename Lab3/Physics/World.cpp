66c9b5d13836b11d763352d7aeeb0de65fe1fafb
Photon Cat <lkstc112233@gmail.com>
2018-11-15 01:19:38 -0500

    Added ball collading detection

--- Lab3/Physics/World.cpp
+++ Lab3/Physics/World.cpp
@@ -14,9 +14,29 @@ void World::timePass(float t) {
   for (auto& c : controllers) {
     c.speed += t * g;
   }
+  // Check wall colliding
   for (auto& w : walls) {
     for (auto& c : controllers) {
       c.speed += t * w.applyForce(c.position) / c.mass;
+    }
+  }
+  // Check balls colliding
+  for (auto i = controllers.begin(); i != controllers.end(); ++i) {
+    for (auto j = i + 1; j != controllers.end(); ++j) {
+      if (glm::distance(i->position, j->position) < 2) {
+        // Bounce
+        auto norm = i->position - j->position;
+        auto projection = [norm](glm::vec3 vec) {
+          float mag = glm::dot(vec, norm);
+          return mag / glm::length(norm) * norm;
+        };
+        auto differ_i_v = projection(i->speed);
+        auto differ_j_v = projection(j->speed);
+        i->speed += differ_j_v;
+        i->speed -= differ_i_v;
+        j->speed += differ_i_v;
+        j->speed -= differ_j_v;
+      }
     }
   }
   for (auto& c : controllers) {
