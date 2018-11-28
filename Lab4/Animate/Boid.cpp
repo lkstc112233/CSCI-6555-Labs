db07f9c73fe505465ba9626063c6b95f2fadeff2
Photon Cat <lkstc112233@gmail.com>
2018-11-28 16:41:00 -0500

    Added constructor, object and draw

--- Lab4/Animate/Boid.cpp
+++ Lab4/Animate/Boid.cpp
@@ -1,1 +1,8 @@
 #include "Boid.h"
+
+Boid::Boid(Object3D& object_operating) : object(object_operating) {}
+
+void Boid::draw(ShaderProgram& shader) {
+  // TODO: put the object to correct position.
+  object.draw(shader);
+}
--- Lab4/Animate/Boid.h
+++ Lab4/Animate/Boid.h
@@ -3,9 +3,16 @@
 
 #include <glm/glm.hpp>
 
+#include "../../Graphics/Object/Object.h"
+#include "../../Graphics/Shaders/Shader.h"
+
 class Boid {
  private:
+  Object3D& object;
+
  public:
+  Boid(Object3D& object);
+  void draw(ShaderProgram& shader);
 };
 
 #endif  // ANIMATE_BOID_H
