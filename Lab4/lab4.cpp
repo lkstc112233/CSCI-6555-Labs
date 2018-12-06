86ff9f222c7729dadd0f303194ab37127a64364b
Photon Cat <lkstc112233@gmail.com>
2018-12-06 02:58:17 -0500

    Darken the background a little.

--- Lab4/lab4.cpp
+++ Lab4/lab4.cpp
@@ -173,7 +173,7 @@ int main(int argc, char** argv) {
 
     // render
     // ------
-    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClearColor(0.1f, 0.15f, 0.15f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     shaderProgram.setMatrix("view", camera.getViewMat());
